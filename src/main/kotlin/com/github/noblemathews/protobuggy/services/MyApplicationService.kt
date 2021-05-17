35427a47cad7df42caa98e48aa21b29261130735
GitHub Action <action@github.com>
2021-05-17 09:57:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/noblemathews/protobuggy/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/noblemathews/protobuggy/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.noblemathews.protobuggy.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noblemathews.protobuggy.MyBundle
 
 class MyApplicationService {
 
